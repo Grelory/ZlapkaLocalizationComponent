f8824973766d207b10e681ddbf41acd47692adff
Grelory <gmpwm99@gmail.com>
2020-12-09 11:27:24 +0100

    add functionality to handle event header crud methods

--- src/main/java/com/codecool/zlapka/localizationcomponent/controllers/LocalizationController.java
+++ src/main/java/com/codecool/zlapka/localizationcomponent/controllers/LocalizationController.java
@@ -1,5 +1,6 @@
 package com.codecool.zlapka.localizationcomponent.controllers;
 
+import com.codecool.zlapka.localizationcomponent.services.EventHeaderService;
 import com.codecool.zlapka.localizationcomponent.services.LocalizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -8,50 +9,116 @@ import org.springframework.web.bind.annotation.*;
 public class LocalizationController {
 
     @Autowired
-    private LocalizationService service;
+    private LocalizationService localizationService;
+    @Autowired
+    private EventHeaderService eventHeaderService;
+
+    @GetMapping(value = "/localization/all")
+    public String getAllLocation() {
+
+        // authentication
+
+        // get appropriate information from http
+
+        return localizationService.getAllLocalizations();
+
+        // return proper status and info
+    }
 
     @GetMapping(value = "/localization")
-    public String getLocation(@RequestParam(name = "id") Long id) {
+    public String getLocation(@RequestParam(name = "id") String id) {
 
-        // todo authentication
+        // authentication
 
-        // todo get appropriate information from http
+        // get appropriate information from http
 
-        return service.getLocalizationById(id);
+        return localizationService.getLocalizationById(id);
 
-        // todo return proper status and info
+        // return proper status and info
     }
 
     @DeleteMapping(value = "/localization")
-    public String deleteLocation(@RequestParam(name = "id") Long id){
-        // todo authentication
+    public String deleteLocation(@RequestParam(name = "id") String id){
+        // authentication
 
-        // todo get appropriate information from http
+        // get appropriate information from http
 
-        return service.deleteLocalizationById(id);
+        return localizationService.deleteLocalizationById(id);
 
-        // todo return proper status and info
+        // return proper status and info
     }
 
     @PutMapping(value = "/localization")
     public String putEvent(@RequestBody String jsonLocalization){
-        // todo authentication
+        // authentication
 
-        // todo get appropriate information from http
-        return service.update(jsonLocalization);
+        // get appropriate information from http
 
-        // todo return proper status and info
+        return localizationService.update(jsonLocalization);
+
+        // return proper status and info
     }
 
     @PostMapping(value = "/localization")
     public String postEvent(@RequestBody String jsonLocalization){
-        // todo authentication
+        // authentication
 
-        // todo get appropriate information from http
-        service.add(jsonLocalization);
+        // get appropriate information from http
 
-        // todo return proper status and info
-        return "{ \"status\":\"200\", \"info\":\"Localization has been updated\"";
+        localizationService.add(jsonLocalization);
+
+        // return proper status and info
+        return "{ \"status\":\"200\", \"info\":\"Localization has been updated\"}";
     }
+
+    @GetMapping(value = "/localization/bond/event")
+    public String getEventsByLocalizationId(@RequestParam(name = "id") String id) {
+
+        // authentication
+
+        // get appropriate information from http
+
+        return eventHeaderService.getEventsLocalizationById(id);
+
+        // return proper status and info
+    }
+
+    @DeleteMapping(value = "/localization/bond/event")
+    public String removeEventFromLocationById(@RequestParam(name = "id") String id){
+        // authentication
+
+        // get appropriate information from http
+
+        return eventHeaderService.deleteById(id);
+
+        // return proper status and info
+    }
+
+    @PutMapping(value = "/localization/bond/event")
+    public String putEventHeaders(@RequestBody String jsonEventHeader){
+        // authentication
+
+        // get appropriate information from http
+
+        return eventHeaderService.update(jsonEventHeader);
+
+        // return proper status and info
+    }
+
+    @PostMapping(value = "/localization/bond/event")
+    public String postEventHeader(@RequestBody String jsonEventHeader){
+        // authentication
+
+        // get appropriate information from http
+
+        return eventHeaderService.bindEventToLocalization(jsonEventHeader);
+
+        // return proper status and info
+    }
+
+
+
+
+
 }
 
